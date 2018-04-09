0a74769d6e254094b30cacc54ee232c237109e88
Justin Lee <justin.lee007@gmail.com>
2018-04-08 22:48:41 -0700

    Updated waypoint_updater for final walkthrough

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -4,10 +4,10 @@ import math
 import numpy as np
 
 import rospy
-from std_msgs.msg import Int32
-from geometry_msgs.msg import PoseStamped, TwistStamped, sys
+from geometry_msgs.msg import PoseStamped
 from scipy.spatial import KDTree
-from styx_msgs.msg import Lane, Waypoint, TrafficLightArray, TrafficLight
+from std_msgs.msg import Int32
+from styx_msgs.msg import Lane, Waypoint
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -113,8 +113,10 @@ class WaypointUpdater(object):
             p = Waypoint()
             p.pose = wp.pose
 
-            stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0)# Two waypoints back from line so front of car stops at line
+            # Two waypoints back from line so front of car stops at line
+            stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0)
             dist = self.distance(waypoints, i, stop_idx)
+            # TODO: make this linear (multiply by constant, continuous derivative, etc)
             vel = math.sqrt(2 * MAX_DECEL * dist)
             if vel < 1.0:
                 vel = 0.0
@@ -158,188 +160,6 @@ class WaypointUpdater(object):
 
     def current_velocity_cb(self, msg):
         self.current_velocity = msg.twist.linear.x
-
-    def convert_local(self, waypoint, current_pos):
-        """
-        Helper function for converting from global to local coordinates
-        :param waypoint:
-        :param current_pos:
-        :return:
-        """
-        # Grab the waypoint and car position variables
-        x_way = waypoint.pose.pose.position.x
-        y_way = waypoint.pose.pose.position.y
-        x_car = current_pos.pose.position.x
-        y_car = current_pos.pose.position.y
-        # Convert from Quarternion to Radians
-        theta_car = 2 * math.acos(current_pos.pose.orientation.w)
-        theta_waypoint = 2 * math.acos(waypoint.pose.pose.orientation.w)
-
-        # Perform coordinate localization
-        x_shift = x_way - x_car
-        y_shift = y_way - y_car
-        x = x_shift * math.cos(0 - theta_car) - y_shift * math.sin(0 - theta_car)
-        y = x_shift * math.sin(0 - theta_car) + y_shift * math.cos(0 - theta_car)
-        return x, y, theta_car, theta_waypoint
-
-    def kph2mps(self, velocity_kph):
-        return (velocity_kph * 1000.) / (60. * 60.)
-
-    def pose_cb(self, msg):
-        """
-        Callback for the current car position received
-        :param msg:
-        :return:
-        """
-        self.current_pos = msg
-        # x = msg.pose.position.x    #Access x value like this
-        # theta = msg.pose.orientation.w # Access w value like this
-
-        if self.base_waypoints is None or self.current_velocity is None:
-            return
-
-        # Create list for the published final_waypoints
-        final_waypoints_list = []
-
-        # Scan through all base_waypoints
-        for i in range(len(self.base_waypoints.waypoints)):
-            waypoint = self.base_waypoints.waypoints[i]
-
-            # Convert waypoint to local coordinates
-            x, y, theta_car, theta_waypoint = self.convert_local(waypoint, self.current_pos)
-            orientation_match = math.cos(theta_waypoint - theta_car)
-
-            # Check if the waypoint is in front of the car, and if the orientation is within +/- pi/4 of the car
-            if x >= 0.00 and orientation_match > 0.707:
-                self.closest_waypoint_idx = i
-
-                if self.red_stop_light_ahead:
-                    dist_to_stop_line = self.distance(self.base_waypoints.waypoints, self.closest_waypoint_idx,
-                                                      self.red_stop_light_waypoint_idx) - STOP_LIGHT_MARGIN
-                    # deceleration = (final_v^2 - curr_v^2) / (2 * distance)
-                    decel = -1 * min((pow(self.current_velocity, 2) / (2 * dist_to_stop_line)), MAX_DECEL)
-                else:
-                    dist_to_stop_line = 0.0
-                    decel = 0.0
-
-                # Track first and last target velocities for logging
-                initial_target_velocity = 0.0
-                target_velocity = 0.0
-
-                # As soon as the first waypoint is found, populate LOOKAHEAD_WPS waypoints into the list sequentially
-                for j in range(LOOKAHEAD_WPS):
-                    j_mod = i + j % self.total_waypoints
-
-                    next_wp = self.base_waypoints.waypoints[j_mod]
-
-                    target_velocity = self.calculate_target_velocity(decel, dist_to_stop_line, j)
-                    if j == 0:
-                        initial_target_velocity = target_velocity
-                    next_wp.twist.twist.linear.x = target_velocity
-                    final_waypoints_list.append(next_wp)
-
-                # Throttle the logging a bit
-                if (i % LOGGING_THROTTLE_FACTOR) == 0:
-                    if self.red_stop_light_ahead:
-                        rospy.logdebug(
-                            "Stop light idx={}, closest idx={}, diff={}, dist_to_stop_line={:.2f}, decel={:.2f}".format(
-                                self.red_stop_light_waypoint_idx, self.closest_waypoint_idx,
-                                self.red_stop_light_waypoint_idx - self.closest_waypoint_idx, dist_to_stop_line, decel))
-
-                    rospy.logdebug(
-                        "Current velocity={:.2f}, target velocity[0]={:.2f}, target velocity[{}]={:.2f}".format(
-                            self.current_velocity, initial_target_velocity, LOOKAHEAD_WPS, target_velocity))
-
-                # Format the message
-                msg = Lane()
-                msg.waypoints = final_waypoints_list
-                # Setting header info to current_pos header
-                msg.header = self.current_pos.header
-                self.final_waypoints_pub.publish(msg)
-                return
-
-    def calculate_target_velocity(self, decel, dist_to_stop_line, j):
-        """
-        Calculates velocity for a given waypoint based on the distance to the stop line and the calculated deceleration.
-        :param decel: static deceleration value to use for the entire set of waypoint trajectories
-        :param dist_to_stop_line: distance in meters to the stop line (includes STOP_LIGHT_MARGIN)
-        :param j: index (out of LOOKAHEAD_WPS)
-        :return: velocity (in m/s)
-        """
-        if self.red_stop_light_ahead and (MIN_BRAKING_DIST < dist_to_stop_line < SAFE_BRAKING_DIST):
-            coast_velocity = (COAST_VELOCITY, 0.0)[dist_to_stop_line < STOP_LINE_MARGIN]
-            calculated_velocity = math.sqrt(-2.0 * decel * dist_to_stop_line) - (j * 0.1)
-            target_velocity = max(coast_velocity, min(calculated_velocity, self.max_speed_mps))
-        else:
-            target_velocity = min((self.current_velocity + (j + 1) * MAX_ACCEL), self.max_speed_mps)
-        if target_velocity < 0.1:
-            target_velocity = 0.0
-        return target_velocity
-
-    def waypoints_cb(self, waypoints):
-        """
-        Callback for base_waypoints when the simulator is started
-        :param self:
-        :param waypoints:
-        :return:
-        """
-
-        # Set a variable for accessing base_waypoints throughout this node
-        self.base_waypoints = waypoints
-        # Set a variable for access the total number of base_waypoints throughout this node
-        self.total_waypoints = len(self.base_waypoints.waypoints)
-        rospy.logdebug("Total waypoints={}".format(self.total_waypoints))
-
-    def get_closest_waypoint_idx(self, start_idx, pose):
-        """
-        Find the index of the traffic light starting from start_idx
-        :param self:
-        :param start_idx:
-        :param pose:
-        :return:
-        """
-        waypoint_idx = -1
-        closest_dist = sys.maxint
-
-        for i in range(self.total_waypoints):
-            waypoint_position = self.base_waypoints.waypoints[(i + start_idx) % self.total_waypoints].pose.pose.position
-            tl_position = pose.pose.position
-            dl = lambda a, b: math.sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2))
-            dist = dl(waypoint_position, tl_position)
-            if dist < closest_dist:
-                waypoint_idx = i
-                closest_dist = dist
-
-        return waypoint_idx
-
-    def traffic_cb(self, msg):
-        if self.current_velocity is None or self.current_pos is None or self.base_waypoints is None:
-            return
-
-        # For testing purpose we are subscribing to the /vehicle/traffic_lights
-        traffic_lights = msg.lights
-        closest_tl = None
-        closest_dist = sys.maxint  # Some big number
-
-        if TEST_MODE_ENABLED:
-            for i in range(len(traffic_lights)):
-                tl = msg.lights[i]
-                if tl.state == TrafficLight.RED or tl.state == TrafficLight.YELLOW:  # Red or Yellow Traffic light
-                    # Convert traffic light location to car local coordinates
-                    x, y, theta_car, theta_tl = self.convert_local(tl, self.current_pos)
-                    dl = lambda a, b: math.sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2))
-                    dist = dl(tl.pose.pose.position,
-                              self.base_waypoints.waypoints[self.closest_waypoint_idx].pose.pose.position)
-                    if x > 0.0 and 0 < dist < closest_dist:
-                        closest_tl = tl
-                        closest_dist = dist
-
-            if closest_tl is not None:
-                tl_waypoint_idx = self.get_closest_waypoint_idx(0, closest_tl.pose)
-                self.red_stop_light_waypoint_idx = tl_waypoint_idx
-                self.red_stop_light_ahead = True
-            else:
-                self.red_stop_light_ahead = False
 
 
 if __name__ == '__main__':
